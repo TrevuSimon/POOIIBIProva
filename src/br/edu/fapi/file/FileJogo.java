4e1446ad02972a893c0805d00d6c54102fa9476f
Guilherme <gui_sc@outlook.com>
2018-09-16 23:17:59 -0300

    Terminando arquivo

--- src/br/edu/fapi/file/FileJogo.java
+++ src/br/edu/fapi/file/FileJogo.java
@@ -3,6 +3,7 @@ package br.edu.fapi.file;
 import br.edu.fapi.jogo.Jogo;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,8 +11,12 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class FileJogo {
-    public void salvarRelatorio(List<Jogo> jogos, String nomeArquivo){
-        String fileName = "c://"+nomeArquivo+".txt";
+    public void salvarRelatorio(List<Jogo> jogos, String nomeArquivo) throws IOException {
+
+        Path path = Paths.get("C:\\Relatorios");
+        Files.createDirectories(path);
+
+        String fileName = "c://Relatorios//"+nomeArquivo+".txt";
         try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileName))) {
             for (Jogo jogo:jogos) {
 
@@ -22,6 +27,13 @@ public class FileJogo {
                 Data e Hora de Início: #Data_Hora_Inicio#
                 Data e Hora de Fim: #Data_Hora_Fim#*/
                 writer.write("Nome: "+ jogo.getJogador());
+                writer.write("\nPalavra: "+ jogo.getPalavra());
+                writer.write("\nResultado: "+ jogo.getResultado());
+                writer.write("\nData e Hora de Início:: "+ jogo.getHoraJogo());
+                writer.write("\nData e Hora de Fim:: "+ jogo.getHoraJogoFim());
+                writer.write("\nNome: "+ jogo.getJogador());
+                writer.write("\n\n");
+
 
             }
         } catch (IOException e) {
